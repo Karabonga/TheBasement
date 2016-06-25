9144e9167685e384e11fddcabfe4ec2758b5115d
woody97gj <k-ph.holz@arcor.de>
2016-06-25 15:11:45 +0200

    add player

--- RetroEngine/Player.cs
+++ RetroEngine/Player.cs
@@ -1,18 +1,61 @@
-﻿using SharpDX.DXGI;
-using SharpDX.Windows;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using SharpDX;
-using System.Windows.Forms;
 
-class Player : GameObject
+namespace RetroEngine
 {
-    private float movespeed;
-    private Vector3 position;
-    private Vector3 rotation;
-    Input input;
-     
-    public Player()
+    class Player : GameObject
     {
-        this.movespeed = NORMAL_MOVESPEED;
+        private Vector3 startposition;
+        private Vector3 rotation;
+        private Vector3 position;
+        private float movespeed;
+        private float rotationspeed;
+        private Camera playercam;
+        private Time time;
+        public Player(Vector3 start, Camera cam, Time time)
+            :base(start,null)
+        {
+            this.startposition = start;
+            this.position = start;
+            this.movespeed = 10f;
+            this.rotationspeed = 10f;
+            this.playercam = cam;
+            this.time = time;
+
+
+        }
+        void handleInput(Input input)
+        {
+            if (input.GetKeyDown("MoveBackward"))
+                this.movespeed = -movespeed;
+            if (input.GetKeyDown("TurnLeft"))
+                this.rotationspeed = -rotationspeed;
+        }
+        void move(Input input)
+        {
+            handleInput(input);
+            this.position += this.Forward * (float)(movespeed * time.DeltaTime);
+            this.rotation = new Vector3(this.Rotation.X, (float)(this.Rotation.Y + rotationspeed * time.DeltaTime), this.Rotation.Z);
+            playercam.Position = position;
+            playercam.Rotation = rotation;
+        }
+        void update(double gameTime)
+        {
+
+        }
+        public Vector3 StartPos
+        {
+            get { return startposition; }
+            set { startposition = value; }
+        }
+        public float Speed
+        {
+            get { return movespeed; }
+            set{ movespeed = value; }
+        }
     }
 }
